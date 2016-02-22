219e200ca93ac6950a2479897325f870b2c3ef7f
Lingduo Kong <lingduokong@Lingduos-MacBook-Pro.local>
2016-02-22 01:12:00 -0800

    new version

---
+++ src/main/java/com/LingduoKong/app/App.java
@@ -0,0 +1,13 @@
+package com.LingduoKong.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+
+        BookReader bookReader = new BookReader();
+    }
+}
---
+++ src/main/java/com/LingduoKong/app/Genre.java
@@ -0,0 +1,24 @@
+package com.LingduoKong.app;
+
+import JSON.JSONObject;
+
+import java.util.HashMap;
+
+/**
+ *  it represents a genre which contains a table of keyword and value
+ */
+
+public class Genre {
+
+    HashMap<String, Integer> key_value;
+    String name;
+    public Genre(String name) {
+        this.name = name;
+        key_value = new HashMap<>();
+    }
+
+    public String toString() {
+        JSONObject genre = new JSONObject(key_value);
+        return genre.toString();
+    }
+}
